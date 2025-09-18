1180197d5ed6ecabb17147650d22fa3dcd51dfd3
Marcossantosbdev <marcosssantosbuenocontato@gmail.com>
2025-09-18 10:51:44 -0300

    Aula 12 - JS e DOM, TV Pink White update

--- Módulo01-HTML-e-CSS/Módulo 2 - Javascript/Aula 11 -  Variáveis e DOM/Marcos/DOM/script.js
+++ Módulo01-HTML-e-CSS/Módulo 2 - Javascript/Aula 11 -  Variáveis e DOM/Marcos/DOM/script.js
@@ -16,6 +16,17 @@ let paragrafo2 = document.getElementById("Caixa2")
 paragrafo2.innerHTML = "Caixa de fruta(ID)"
 paragrafo2.style.backgroundColor = "red"
 
-let paragrafo3 = document.getElementsByClassName("Caixa3")[0]
-paragrafo3[0].innerHTML = "Caixa de verduras(classe)"
-paragrafo3[0].style.backgroundColor = "blue"
+let paragrafo3 = document.getElementsByClassName("Caixa")[0]
+paragrafo3.innerHTML = "Caixa de verduras(classe)"
+paragrafo3.style.backgroundColor = "blue"
+
+//let paragrafo4 = document.getElementsByName("Caixa4")[3]
+//paragrafo4.placeholder = "Digite Caixa de legumes(Por nome)"
+
+let paragrafo4 = document.querySelector("#Caixa4")
+paragrafo4.innerHTML = "Caixa de Legumes(Query)"
+paragrafo4.style.backgroundColor = "purple"
+
+let paragrafo5 = document.querySelectorAll("p")[4]
+paragrafo5.innerHTML = "Caixa HortiFrutti(Query selector all)"
+paragrafo5.style.backgroundColor = "lightgreen"
