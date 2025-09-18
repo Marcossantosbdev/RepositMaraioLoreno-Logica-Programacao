e4de19ffb3487091cf656e785fd4b04f75c2edab
Marcossantosbdev <marcosssantosbuenocontato@gmail.com>
2025-09-18 09:46:57 -0300

    Aula 11 - Tricky update

---
+++ Módulo01-HTML-e-CSS/Módulo 2 - Javascript/Aula 11 -  Variáveis e DOM/Marcos/DOM/script.js
@@ -0,0 +1,9 @@
+// window.document.body.style.backgroundColor = "pink";
+// document.body.style.color = "white";
+
+// window.alert("seja bem vindo");
+
+// window.prompt("Login");
+//window.prompt("senha");
+
+// window.confirm("Pink+white -  Frank Ocean"); 
