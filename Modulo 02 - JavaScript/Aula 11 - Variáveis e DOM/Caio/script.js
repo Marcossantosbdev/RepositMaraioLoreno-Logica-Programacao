69418bcfeada6f0542f171b1b1a607b55fb3eda1
Loren-1psum <Caioloren28@gmail.com>
2025-09-19 09:22:37 -0300

    New Classes Update
    Update.

---
+++ Modulo 02 - JavaScript/Aula 11 - Variáveis e DOM/Caio/script.js
@@ -0,0 +1,17 @@
+console.log("String " + "Hello World");
+console.log("Número " + (2 + 2));
+console.log("Booleano" + true);
+console.log("Hello World");
+
+
+// var (Variável de Escopo Global)
+var nome = "Matheus"
+nome = "Robson"
+console.log(nome);
+
+//let (Variável de Escopo Local)
+let  sobrenome = "Souza"
+sobrenome = "Silva"
+
+//Constante (Valor que não pode ser alterado)
+const pi = 3.14
